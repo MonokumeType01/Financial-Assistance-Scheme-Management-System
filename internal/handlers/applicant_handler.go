dfcb8d0c14e318a356c5c720ffab7a5731f4a878
MonokumeType01 <wongshengxiang@gmail.com>
2025-03-23 01:26:15 +0800

    Add: Applicant Update and Delete By ID on Service and Handler Functions

--- internal/handlers/applicant_handler.go
+++ internal/handlers/applicant_handler.go
@@ -3,9 +3,8 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/MonokumeType01/Financial-Assistance-Scheme-Management-System/internal/models"
+	"github.com/MonokumeType01/Financial-Assistance-Scheme-Management-System/internal/dto"
 	"github.com/MonokumeType01/Financial-Assistance-Scheme-Management-System/internal/services"
-	"github.com/MonokumeType01/Financial-Assistance-Scheme-Management-System/internal/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,35 +19,85 @@ func NewApplicantHandler(service *services.ApplicantService) *ApplicantHandler {
 
 // CREATE Applicant with Household
 func (h *ApplicantHandler) CreateApplicant(c *gin.Context) {
-	var data models.ApplicantWithHouseHold
+	var data dto.ApplicantWithHousehold
 
 	if err := c.ShouldBindJSON(&data); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create applicant",
+			"details": err.Error()})
 		return
-	}
-
-	data.ID = utils.GenerateUUID()
-	for i := range data.Household {
-		data.Household[i].ID = utils.GenerateUUID()
-		data.Household[i].ApplicantID = data.ID
 	}
 
 	if err := h.Service.RegisterApplicantWithHousehold(&data); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register applicant"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register applicant",
+			"details": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{"message": "Applicant registered successfully", "applicant": data})
+	c.JSON(http.StatusCreated, gin.H{"message": "Applicant registered successfully"})
 }
 
-// GET Applicant with Household
+// RETRIEVE Applicant with Household
 func (h *ApplicantHandler) GetApplicant(c *gin.Context) {
 	id := c.Param("id")
-	applicant, err := h.Service.GetApplicantWithHousehold(id)
+	applicant, err := h.Service.GetApplicantWithID(id)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Applicant not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "Applicant not found",
+			"details": err.Error()})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"applicant": applicant})
+}
+
+// RETRIEVE All Applicants
+func (h *ApplicantHandler) GetAllApplicants(c *gin.Context) {
+	applicants, err := h.Service.GetApplicants(c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve applicants",
+			"details": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"applicants": applicants})
+}
+
+// UDPATE applicant by ID
+func (h *ApplicantHandler) UpdateApplicant(c *gin.Context) {
+	id := c.Param("id")
+
+	var data dto.ApplicantWithHousehold
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Invalid input format",
+			"details": err.Error(),
+		})
+		return
+	}
+
+	if err := h.Service.UpdateApplicant(id, &data); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":   "Failed to update applicant",
+			"details": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Applicant updated successfully"})
+}
+
+// DELETE Applicant By ID
+func (h *ApplicantHandler) DeleteApplicant(c *gin.Context) {
+	id := c.Param("id")
+
+	if err := h.Service.DeleteApplicant(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":   "Failed to delete applicant",
+			"details": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Applicant deleted successfully",
+	})
 }
--- internal/services/applicant_service.go
+++ internal/services/applicant_service.go
@@ -1,6 +1,10 @@
 package services
 
 import (
+	"context"
+	"errors"
+
+	"github.com/MonokumeType01/Financial-Assistance-Scheme-Management-System/internal/dto"
 	"github.com/MonokumeType01/Financial-Assistance-Scheme-Management-System/internal/models"
 	"github.com/MonokumeType01/Financial-Assistance-Scheme-Management-System/internal/utils"
 	"gorm.io/gorm"
@@ -15,19 +19,36 @@ func NewApplicantService(db *gorm.DB) *ApplicantService {
 }
 
 // CREATE Applicant with Household Members
-func (s *ApplicantService) RegisterApplicantWithHousehold(data *models.ApplicantWithHouseHold) error {
+func (s *ApplicantService) RegisterApplicantWithHousehold(data *dto.ApplicantWithHousehold) error {
+	applicant := models.Applicant{
+		ID:               utils.GenerateUUID(),
+		Name:             data.Name,
+		EmploymentStatus: data.EmploymentStatus,
+		Sex:              data.Sex,
+		DateOfBirth:      data.DateOfBirth,
+	}
+
+	householdMembers := make([]models.HouseholdMember, len(data.Household))
+	for i, member := range data.Household {
+		householdMembers[i] = models.HouseholdMember{
+			ID:               utils.GenerateUUID(),
+			Name:             member.Name,
+			EmploymentStatus: member.EmploymentStatus,
+			Sex:              member.Sex,
+			DateOfBirth:      member.DateOfBirth,
+			Relation:         member.Relation,
+			SchoolLevel:      member.SchoolLevel,
+			ApplicantID:      applicant.ID,
+		}
+	}
+
 	return s.DB.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Create(&data.Applicant).Error; err != nil {
+		if err := tx.Create(&applicant).Error; err != nil {
 			return err
 		}
 
-		for i := range data.Household {
-			data.Household[i].ID = utils.GenerateUUID()
-			data.Household[i].ApplicantID = data.ID
-		}
-
-		if len(data.Household) > 0 {
-			if err := tx.Create(&data.Household).Error; err != nil {
+		if len(householdMembers) > 0 {
+			if err := tx.Create(&householdMembers).Error; err != nil {
 				return err
 			}
 		}
@@ -36,21 +57,162 @@ func (s *ApplicantService) RegisterApplicantWithHousehold(data *models.Applicant
 	})
 }
 
-// RETRIEVE Applicant with Household Members
-func (s *ApplicantService) GetApplicantWithHousehold(id string) (*models.ApplicantWithHouseHold, error) {
+// RETRIEVE All Applicant with Household Members
+func (s *ApplicantService) GetApplicants(ctx context.Context) ([]dto.ApplicantWithHousehold, error) {
+	var applicants []models.Applicant
+	if err := s.DB.Preload("Household").Find(&applicants).Error; err != nil {
+		return nil, err
+	}
+
+	output := make([]dto.ApplicantWithHousehold, len(applicants))
+	for i, applicant := range applicants {
+		householdDTO := make([]dto.HouseholdMember, len(applicant.Household))
+		for j, member := range applicant.Household {
+			householdDTO[j] = dto.HouseholdMember{
+				ID:               member.ID,
+				Name:             member.Name,
+				EmploymentStatus: member.EmploymentStatus,
+				Sex:              member.Sex,
+				DateOfBirth:      member.DateOfBirth,
+				Relation:         member.Relation,
+			}
+		}
+
+		output[i] = dto.ApplicantWithHousehold{
+			Applicant: dto.Applicant{
+				ID:               applicant.ID,
+				Name:             applicant.Name,
+				EmploymentStatus: applicant.EmploymentStatus,
+				Sex:              applicant.Sex,
+				DateOfBirth:      applicant.DateOfBirth,
+			},
+			Household: householdDTO,
+		}
+	}
+
+	return output, nil
+}
+
+// RETRIEVE Applicant with Household Members by Applicant ID
+func (s *ApplicantService) GetApplicantWithID(id string) (*dto.ApplicantWithHousehold, error) {
 	var applicant models.Applicant
 	var household []models.HouseholdMember
 
 	if err := s.DB.First(&applicant, "id = ?", id).Error; err != nil {
-		return nil, err
+		return nil, errors.New("applicant not found")
 	}
 
 	if err := s.DB.Find(&household, "applicant_id = ?", id).Error; err != nil {
-		return nil, err
+		return nil, errors.New("failed to retrieve household members")
 	}
 
-	return &models.ApplicantWithHouseHold{
-		Applicant: applicant,
-		Household: household,
+	householdDTO := make([]dto.HouseholdMember, len(household))
+	for i, member := range household {
+		householdDTO[i] = dto.HouseholdMember{
+			ID:               member.ID,
+			Name:             member.Name,
+			EmploymentStatus: member.EmploymentStatus,
+			Sex:              member.Sex,
+			DateOfBirth:      member.DateOfBirth,
+			Relation:         member.Relation,
+		}
+	}
+
+	return &dto.ApplicantWithHousehold{
+		Applicant: dto.Applicant{
+			ID:               applicant.ID,
+			Name:             applicant.Name,
+			EmploymentStatus: applicant.EmploymentStatus,
+			Sex:              applicant.Sex,
+			DateOfBirth:      applicant.DateOfBirth,
+		},
+		Household: householdDTO,
 	}, nil
+}
+
+// UDPATE applicant by ID
+func (s *ApplicantService) UpdateApplicant(id string, updatedData *dto.ApplicantWithHousehold) error {
+	var applicant models.Applicant
+
+	if err := s.DB.First(&applicant, "id = ?", id).Error; err != nil {
+		return errors.New("applicant not found")
+	}
+
+	return s.DB.Transaction(func(tx *gorm.DB) error {
+		applicant.Name = updatedData.Name
+		applicant.EmploymentStatus = updatedData.EmploymentStatus
+		applicant.Sex = updatedData.Sex
+		applicant.DateOfBirth = updatedData.DateOfBirth
+
+		if err := tx.Save(&applicant).Error; err != nil {
+			return err
+		}
+
+		if err := tx.Where("applicant_id = ?", applicant.ID).Delete(&models.HouseholdMember{}).Error; err != nil {
+			return err
+		}
+
+		existingHouseholdIDs := make(map[string]bool)
+		for _, existingMember := range applicant.Household {
+			existingHouseholdIDs[existingMember.ID] = true
+		}
+
+		householdMembers := make([]models.HouseholdMember, len(updatedData.Household))
+		for i, member := range updatedData.Household {
+			if existingHouseholdIDs[member.ID] {
+				householdMembers[i] = models.HouseholdMember{
+					ID:               member.ID,
+					Name:             member.Name,
+					EmploymentStatus: member.EmploymentStatus,
+					Sex:              member.Sex,
+					DateOfBirth:      member.DateOfBirth,
+					Relation:         member.Relation,
+					SchoolLevel:      member.SchoolLevel,
+					ApplicantID:      applicant.ID,
+				}
+			} else {
+				householdMembers[i] = models.HouseholdMember{
+					ID:               utils.GenerateUUID(),
+					Name:             member.Name,
+					EmploymentStatus: member.EmploymentStatus,
+					Sex:              member.Sex,
+					DateOfBirth:      member.DateOfBirth,
+					Relation:         member.Relation,
+					SchoolLevel:      member.SchoolLevel,
+					ApplicantID:      applicant.ID,
+				}
+			}
+		}
+
+		if len(householdMembers) > 0 {
+			if err := tx.Create(&householdMembers).Error; err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+}
+
+// DELETE Applicant By ID
+func (s *ApplicantService) DeleteApplicant(id string) error {
+	var applicant models.Applicant
+	if err := s.DB.First(&applicant, "id = ?", id).Error; err != nil {
+		return errors.New("applicant not found")
+	}
+
+	if err := s.DB.Where("applicant_id = ?", id).Delete(&models.HouseholdMember{}).Error; err != nil {
+		return errors.New("failed to delete household members")
+	}
+
+	// TODO: Add after applicant endpoints are done
+	// if err := s.DB.Where("applicant_id = ?", id).Delete(&models.Application{}).Error; err != nil {
+	// 	return errors.New("failed to delete applicant's application")
+	// }
+
+	if err := s.DB.Delete(&applicant).Error; err != nil {
+		return errors.New("failed to delete applicant")
+	}
+
+	return nil
 }
