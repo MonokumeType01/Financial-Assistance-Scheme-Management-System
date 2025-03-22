5fe2362a6ed1927de6bf92cfc57f633cb987a9e2
MonokumeType01 <wongshengxiang@gmail.com>
2025-03-22 11:09:43 +0800

    Refactor and Update: main.go
    - Extracted service initialization logic into initializeServices()
    - Refactor port retrieval into a function
    - Added shutdown function, SchemeService and SchemeHandler

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/MonokumeType01/Financial-Assistance-Scheme-Management-System/config"
 	"github.com/MonokumeType01/Financial-Assistance-Scheme-Management-System/internal/handlers"
@@ -22,17 +27,55 @@ func main() {
 	router := gin.Default()
 
 	// Services & Handlers
-	applicantService := services.NewApplicantService(config.DB)
+	applicantService, schemeService := initializeServices()
 	applicantHandler := handlers.NewApplicantHandler(applicantService)
+	schemeHandler := handlers.NewSchemeHandler(schemeService)
 
 	// Routes
-	routes.SetupRoutes(router, applicantHandler)
+	routes.SetupRoutes(router, applicantHandler, schemeHandler)
 
+	srv := &http.Server{
+		Addr:    ":" + getPort(),
+		Handler: router,
+	}
+
+	//run server
+	go func() {
+		log.Printf("Server is running on port %s", getPort())
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	shutdown(srv)
+}
+
+func initializeServices() (*services.ApplicantService, *services.SchemeService) {
+	applicantService := services.NewApplicantService(config.DB)
+	schemeService := services.NewSchemeService(config.DB)
+	return applicantService, schemeService
+}
+
+func getPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	return port
+}
 
-	log.Printf("Server is running on port %s", port)
-	router.Run(":" + port)
+func shutdown(srv *http.Server) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exited.")
 }
